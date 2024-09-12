20aea5b3684805c2758b765b0601f895f80fdf45
wekers <fernando@wekers.org>
2024-09-12 20:27:55 -0300

    Test -> @ BeforeEach

--- backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTest.java
@@ -2,25 +2,33 @@ package com.devsuperior.dscatalog.repositories;
 
 import com.devsuperior.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @DataJpaTest
 class ProductRepositoryTest {
 
+    private long existingId;
+    private long nonExistingId;
+
     @Autowired
     private ProductRepository repository;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 100L;
+    }
+
 
     @Test
     public void deleteShouldDeleteObjectWhenIdExists(){
 
         // arrange
-        long existingId = 1L;
 
         // action
         repository.deleteById(existingId);
