d230764e15c96feca7f88e8bbd985a5c56e5cd0e
wekers <fernando@wekers.org>
2024-09-16 18:40:56 -0300

    Test -> Mockito -> deleteShouldThrowDatabaseExceptionWhenDependentId

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -22,16 +24,21 @@ class ProductServiceTest {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
     private long countTotalProducts;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 100L;
+        dependentId = 4L;
         countTotalProducts = 25L;
 
         Mockito.when(repository.existsById(existingId)).thenReturn(true);
         Mockito.when(repository.existsById(nonExistingId)).thenReturn(false);
+        Mockito.when(repository.existsById(dependentId)).thenReturn(true);
+
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
 
     }
 
@@ -52,6 +59,13 @@ class ProductServiceTest {
             service.delete(nonExistingId);
         });
 
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId(){
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            service.delete(dependentId);
+        });
     }
 
 }
