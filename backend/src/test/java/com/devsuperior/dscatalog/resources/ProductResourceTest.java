540be1e11c67a684ec1bcc771d0c72d5a9e8d17e
wekers <fernando@wekers.org>
2024-09-19 19:10:18 -0300

    Test -> Camada Web -> Mockito resource.findAll -> findAllShouldReturnPage()

---
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTest.java
@@ -0,0 +1,49 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.services.ProductService;
+import com.devsuperior.dscatalog.tests.Factory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get; //tem que colocar na mão
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status; //tem que colocar na mão
+
+
+@WebMvcTest(ProductResource.class)
+class ProductResourceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService service;
+
+    private ProductDTO productDTO;
+    private PageImpl<ProductDTO> page;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+
+        productDTO = Factory.createProductDTO();
+
+        //List products = List.of(productDTO);
+        //Pageable pageable = PageRequest.of(0, 10);
+       // PageImpl page = new PageImpl<>(products, pageable, products.size());
+
+        Mockito.when(service.findAllPaged(ArgumentMatchers.any())).thenReturn(page);
+    }
+
+    @Test
+    public void findAllShouldReturnPage() throws Exception{
+        mockMvc.perform(get("/products")).andExpect(status().isOk());
+
+    }
+
+}
