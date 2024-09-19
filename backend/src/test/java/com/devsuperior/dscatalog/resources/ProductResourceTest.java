6914e2bb1a9e5094eb7b65b0ef7cc588f65e2b14
wekers <fernando@wekers.org>
2024-09-19 20:26:40 -0300

    Test -> Camada Web -> Mockito resource.findAll -> findAllShouldReturnPage() - Refactor

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTest.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceTest.java
@@ -11,7 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get; //tem que colocar na mão
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status; //tem que colocar na mão
 
@@ -42,7 +45,10 @@ class ProductResourceTest {
 
     @Test
     public void findAllShouldReturnPage() throws Exception{
-        mockMvc.perform(get("/products")).andExpect(status().isOk());
+        ResultActions result = mockMvc.perform(get("/products")
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
 
     }
 
